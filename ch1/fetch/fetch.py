dae544fc62017f28e015f4d765d941267b30c0b6
Luciano Ramalho <luciano@ramalho.org>
2025-07-03 22:47:20 -0300

    Python rewrite

---
+++ ch1/fetch/fetch.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Fetch prints the content found at each specified URL.
+Python translation of the Go program from:
+The Go Programming Language by Alan A. A. Donovan & Brian W. Kernighan
+"""
+
+import sys
+import httpx
+
+
+def main():
+    if len(sys.argv) < 2:
+        print('Usage: python fetch.py <url1> [url2] ...', file=sys.stderr)
+        sys.exit(1)
+
+    for url in sys.argv[1:]:
+        try:
+            with httpx.Client() as client:
+                response = client.get(url)
+                response.raise_for_status()  # Raise exception for HTTP errors
+                print(response.text, end='')
+        except httpx.HTTPError as e:
+            print(f'fetch: {e}', file=sys.stderr)
+            sys.exit(1)
+        except Exception as e:
+            print(f'fetch: reading {url}: {e}', file=sys.stderr)
+            sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
